6571ff643e356619a1b0d0e6655d764740cb67d7
Gaurav Kumar <Gauravkakar009@gmail.com>
2026-07-01 19:50:44 +0530

    Assignment 1

--- typescript-training/assignments/assignment6.ts
+++ typescript-training/assignments/assignment6.ts
@@ -1,17 +1,22 @@
-// Define the number
+// Assign "n" a value
+    let n : number = 6;
 
-let n : number = 8;
-let flag : boolean = false;
+// Set Flag as False
+    let flag : boolean = false;
 
-for(let i : number = 2; i<n; i++){
-    if(n%i==0){
-        flag = true;
-        break;
+// Run the for loop starting from 2 as from 1 every number is divided to check if "n" is divided by any number.
+    for(let i : number = 2; i<n; i++){
+        if(n%i==0){
+            flag = true;
+            break;
+        }
     }
-    }
+
+// if-else condition to print the number if it is prime or not a prime.
     if(flag==false){
-        console.log("Prime number: " +n)
-
+        console.log("Prime number: " +n);
     }else{
-        console.log("Not a Prime number: " +n)
+        console.log("Not a Prime number: " +n )
     }
+
+
